9475b2eedcb6a3f18a04ef33a69e0d1546abad01
William S. Brandin <william.brandin@mac-pro.local>
2017-09-18 10:53:34 -0500

    initial push

--- SchoolConnect/Singles/AppDelegate.swift
+++ SchoolConnect/Singles/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().largeTitleTextAttributes =
+            [
+            NSAttributedStringKey.foregroundColor: UIColor.white
+            ]
+        
         return true
     }
 
