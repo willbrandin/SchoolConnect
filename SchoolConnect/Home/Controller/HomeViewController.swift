e9732ccbe75040d561187d0814f2eaed9a1e7112
willbrandin <william.brandin@gmail.com>
2017-10-05 09:43:44 -0500

    home screen ui

---
+++ SchoolConnect/Home/Controller/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  SchoolConnect
+//
+//  Created by William S. Brandin on 10/4/17.
+//  Copyright © 2017 William S. Brandin. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
