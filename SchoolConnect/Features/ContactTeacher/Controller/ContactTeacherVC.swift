00a4f4c7eb111d00cdf59a64e23b3f230f46436d
William Brandin <william.brandin@lifeschools.net>
2017-11-09 09:39:21 -0600

    Storyboard for Form

--- SchoolConnect/Features/ContactTeacher/Controller/ContactTeacherVC.swift
+++ SchoolConnect/Features/ContactTeacher/Controller/ContactTeacherVC.swift
@@ -8,23 +8,25 @@
 
 import UIKit
 
-class ContactTeacherVC: UITableViewController {
+private let contactFormIdentifier = "TeacherContact"
+
+class ContactTeacherVC: UITableViewController { 
     
-    //MARK: Properties
-    
-    //MARK: VC Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.blue
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: contactFormIdentifier) as? TeacherContactCell {
+            return cell
+        }
         return UITableViewCell()
+        
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
 }
---
+++ SchoolConnect/Features/ContactTeacher/Controller/TeacherContactCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeacherContactCell.swift
+//  SchoolConnect
+//
+//  Created by William S. Brandin on 11/9/17.
+//  Copyright © 2017 William S. Brandin. All rights reserved.
+//
+
+import UIKit
+
+class TeacherContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
